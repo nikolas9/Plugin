eca0427ef42d56f7b1d2a692cb4940a647a041ea
Nikolas <nikolas9-cool@ya.ru>
2017-02-20 20:46:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nikolas on 20.02.2017.
+ */
+
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        captions: true,
+        auto: true,
+        autoControls: true,
+        pagerCustom: '#bx-pager'
+    });
+});
